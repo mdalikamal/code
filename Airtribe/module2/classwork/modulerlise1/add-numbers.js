e6d7aa81c39953797bc29366b2144292598d43a7
silent martial <154119891+mdalikamal@users.noreply.github.com>
2024-02-22 06:21:34 +0000

    push

---
+++ Airtribe/module2/classwork/modulerlise1/add-numbers.js
@@ -0,0 +1,27 @@
+//1st
+function addNumbers(a, b) {
+  const r = a + b;
+  console.log(`sum of the ${a} and ${b} is ${r}`);
+}
+//console.log(addNumbers(2, 4));
+module.exports = addNumbers;
+//console.log(module);
+
+/*.../Airtribe/module2/classwork/modulerlise1 (main) $ node add-numbers.js
+{
+  id: '.',
+  path: '/workspaces/code/Airtribe/module2/classwork/modulerlise1',
+  exports: {},
+  filename: '/workspaces/code/Airtribe/module2/classwork/modulerlise1/add-numbers.js',
+  loaded: false,
+  children: [],
+  paths: [
+    '/workspaces/code/Airtribe/module2/classwork/modulerlise1/node_modules',
+    '/workspaces/code/Airtribe/module2/classwork/node_modules',
+    '/workspaces/code/Airtribe/module2/node_modules',
+    '/workspaces/code/Airtribe/node_modules',
+    '/workspaces/code/node_modules',
+    '/workspaces/node_modules',
+    '/node_modules'
+  ]
+}*/
---
+++ Airtribe/module2/classwork/modulerlise1/app.js
@@ -0,0 +1,2 @@
+const add = require("./add-numbers.js");
+add(5, 10);
