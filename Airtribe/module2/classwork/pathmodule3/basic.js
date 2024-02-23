78a137ba841233ec8626f0ee37ecfefa0101a86f
silent martial <154119891+mdalikamal@users.noreply.github.com>
2024-02-23 11:09:39 +0000

    modified more into "/pathmodule3/read/"

--- Airtribe/module2/classwork/pathmodule3/basic.js
+++ Airtribe/module2/classwork/pathmodule3/basic.js
@@ -1,1 +1,5 @@
-sbgiuwhouih hskljhsohgoih2jkhbbiuwghuhg
+const Rs = require("path");
+console.log(Rs.basename("Airtribe/module2/classwork/pathmodule3/basic.js"));
+console.log(Rs.dirname("module2/classwork/pathmodule3/basic.js"));
+console.log(Rs.extname("module2/classwork/pathmodule3/basic.js"));
+console.log(Rs.parse("module2/classwork/pathmodule3/basic.js"));
