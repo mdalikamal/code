59bd31cec57e05c180d8ca74560df0b0725b6e1b
silent martial <154119891+mdalikamal@users.noreply.github.com>
2024-03-08 23:24:23 +0530

    modele 3

--- Airtribe/module3/classwork/Airtribe_file_manager_1.1.0_middle_ware/index.js
+++ Airtribe/module3/classwork/Airtribe_file_manager_1.1.0_middle_ware/index.js
@@ -1,82 +1,80 @@
-const fs=require('fs')
+const fs = require("fs");
 
-
-
-function readSyncWriteSync(){
-console.log("read sync");
-const data =fs.readFileSync('./AFM-source/file.txt',{encoding:'utf8',flag:'r'});
-console.log(data);
-fs.writeFileSync('./AFM-destination/output_Sync_read_write_Sync.txt',data,{encoding:'utf8',flag:'w'});
-console.log("data writen");
+function readSyncWriteSync() {
+  console.log("read sync");
+  const data = fs.readFileSync("./AFM-source/file.txt", {
+    encoding: "utf8",
+    flag: "r",
+  });
+  console.log(data);
+  fs.writeFileSync("./AFM-destination/output_Sync_read_write_Sync.txt", data, {
+    encoding: "utf8",
+    flag: "w",
+  });
+  console.log("data writen");
 }
-readSyncWriteSync();
-  function readAsync_writesync(){
-          fs.readFile('./AFM-source/file.txt',"utf-8", (err, data)=>{
-                    if(!err)
-                    console.log(data)
-          let ata =data+`reading file async writing sync `
-          fs.writeFileSync('./AFM-destination/output_ASync_read_write_Sync.txt',ata,{encoding:'utf8',flag:'w'});
-                    console.log("data writen");
-          });
-          
-              
 
+function readAsync_writesync() {
+  fs.readFile("./AFM-source/file.txt", "utf-8", (err, data) => {
+    if (!err) console.log(`readAsync_writesync` + data);
+    let ata = data + `read``ing file async writing sync `;
+    fs.writeFileSync(
+      "./AFM-destination/output_ASync_read_write_Sync.txt",
+      ata,
+      { encoding: "utf8", flag: "w" }
+    );
+    console.log("readAsync_writesync data writen");
+  });
+}
 
+function readFileSync_writeFileAsync() {
+  console.log("readFileSync_writeFileAsync read sync");
+  const data = fs.readFileSync("./AFM-source/file.txt", {
+    encoding: "utf8",
+    flag: "r",
+  });
+  console.log(data);
+  fs.writeFile(
+    "./AFM-destination/output_Sync_read_write_aSync.txt",
+    data,
+    { encoding: "utf8", flag: "w" },
+    function (err, data) {
+      if (err) {
+        console.log("data had not syn");
+      } else {
+        console.log("synced");
+      }
+    }
+  );
+  console.log("every thing done");
+}
 
+function read_Async_write_Async() {
+  const data = fs.readFile(
+    "./AFM-source/file.txt",
+    "utf-8",
+    function (err, data) {
+      if (!err) console.log("1" + data);
+      let r = data + " read file a sync and write file a sync";
 
-
+      console.log(r);
+      fs.writeFile(
+        "./AFM-destination/output_ASync_read_write_ASync.txt",
+        r,
+        { encoding: "utf8", flag: "w" },
+        function (err, data) {
+          if (err) {
+            console.log("data had syn");
+          } else {
+            console.log("synced");
+          }
+        }
+      );
+      console.log("every thing done");
+    }
+  );
 }
 readAsync_writesync();
-function readFileSync_writeFileAsync()
-          {
-          console.log("read sync");
-          const data =fs.readFileSync('./AFM-source/file.txt',{encoding:'utf8',flag:'r'});
-          console.log(data);
-          fs.writeFile('./AFM-destination/output_Sync_read_write_aSync.txt',data,{encoding:'utf8',flag:'w'},function(err,data) 
-                             {
-                              if(err){
-                                        console.log('data had syn');
-                              }
-                              else{
-                               console.log("synced")
-                              }
-                       
-                    })
-                    console.log("every thing done");
-          }
-          readFileSync_writeFileAsync();
-          
-function read_Async_write_Async()
-{
-
- const data= fs.readFile('./AFM-source/file.txt',"utf-8", function(err, data){
-    if(!err)
-    console.log("1"+data);
-    let r=data+" read file a sync and write file a sync"
-  
-  console.log(r)
-  fs.writeFile('./AFM-destination/output_ASync_read_write_ASync.txt',r,{encoding:'utf8',flag:'w'},function(err,data) 
-  {
-                              if(err){
-                                        console.log('data had syn');
-                              }
-                              else{
-                               console.log("synced")
-                              }
-                       
-                    })
-                    console.log("every thing done");
-  
-  
-  
-  
-  
-  
-  
-  
-  })
- 
-
-  
-}
-read_Async_write_Async();
+// readFileSync_writeFileAsync();
+//read_Async_write_Async();
+//readSyncWriteSync();
